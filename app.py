99dbbdab82ffc7afe52b55e5a65c75256a52226e
nagarajupalagani <nagarajupalagani77@gmail.com>
2026-09-16 20:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def submit():
     c.execute("INSERT INTO results(name,roll,score,correct,wrong,unattempted,time_taken,answers) VALUES(?,?,?,?,?,?,?,?)",
               (name,roll,score,correct,wrong,un,elapsed,json.dumps(answers)))
     c.commit()
-    result_id = c.lastrowid
+    result_id = c.execute("SELECT last_insert_rowid()").fetchone()[0]
     c.close()
 
     return jsonify({
